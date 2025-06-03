49a40baf97d15f1f9859b513e833a5c159c83dca
WalneiOliveira <woliveiraassis@gmail.com>
2025-06-03 12:33:43 -0300

    creating main loop connecting Menu and Game classes

--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-import pygame
+from code.game import Game
 
-# starting Pygame
-pygame.init()
-
-# setting window size
-print('setup start')
-screen = pygame.display.set_mode(size=(600, 480))
-# setting screen background color (RGB: white blue)
-screen.fill((135, 206, 235))
-pygame.display.flip()
-pygame.display.set_caption("Montain Shooter Game")
-print('setup end')
-
-print('loop start')
-# main loop
-running = True
-while running:
-    # checking all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-# ending Pygame
-print('quiting...')
-pygame.quit()
+game = Game()
+game.run()
