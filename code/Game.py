43097af27d8f11938ebcf8cca64f74d78b1523fc
WalneiOliveira <woliveiraassis@gmail.com>
2025-06-07 17:23:52 -0300

    adding player 2 mode adding enemy

--- code/Game.py
+++ code/Game.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
@@ -18,12 +20,13 @@ class Game:
             menu_return = menu.run()
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
-                level = Level(self.window, 'level1', menu_return)
+                level = Level(self.window, 'Level1', menu_return)
                 level_return = level.run()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close Window
                 quit()  # end pygame
             else:
-                pass
+                pygame.quit()
+                sys.exit()
 
 
