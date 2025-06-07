d6a7cecd2763e9d0531d1177d4108d66c583f62a
WalneiOliveira <woliveiraassis@gmail.com>
2025-06-07 18:16:00 -0300

    improving main

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
+import sys
+import pygame
 from code.Game import Game
 
-game = Game()
-game.run()
+try:
+    import curses
+except ImportError:
+    try:
+        import windows_curses as curses
+    except ImportError:
+        curses = None
+
+
+def main():
+    game = Game()
+    game.run()
+
+    # Garante que o pygame será encerrado corretamente
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
