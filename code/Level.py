29346c4eb69f9f7f001a1e3b0b81a61130cea7e0
WalneiOliveira <woliveiraassis@gmail.com>
2025-06-08 12:16:48 -0300

    creating mediator patern enemies destruction

--- code/Level.py
+++ code/Level.py
@@ -9,6 +9,7 @@ from pygame.font import Font
 from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
+from code.EntityMediator import EntityMediator
 
 
 class Level:
@@ -43,6 +44,9 @@ class Level:
                     self.entity_list.append(EntityFactory.get_entity(choice))
 
             pygame.display.flip()
+            # colisions and health
+            EntityMediator.verify_collision(entity_list=self.entity_list)
+            EntityMediator.verify_health(entity_list=self.entity_list)
 
             # printed text
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000:.1f}s', C_WHITE, (10, 5))
